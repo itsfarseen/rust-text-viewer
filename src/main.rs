e353694b9b545ede7864da34fb14fb38a57fd6fe
Farseen <oss@itsfarseen.anonaddy.me>
2022-11-26 22:03:11 +0530

    View, scroll; hardcoded font and text

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use std::{collections::VecDeque, time::SystemTime};
 
-const FONT_PATH: &str = "/usr/share/fonts/truetype/iosevka/iosevka-regular.ttf";
+use sdl2::{
+    event::Event, keyboard::Keycode, mouse::MouseWheelDirection, pixels::Color, rect::Rect,
+    render::Canvas, video::Window,
+};
+
+use freetype as ft;
+use texture_atlas::TextureAtlas;
+
+const FONT_PATH: &str =
+    "/nix/store/rqs3nbgwi83a1bv3swxy4jjbg5aibfyc-iosevka-15.6.3/share/fonts/truetype/iosevka-regular.ttf";
+
+const TEXT: &str = include_str!("../small.txt");
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
     let window = video
-        .window("Text Viewer", 800, 600)
+        .window("Text Viewer", 1200, 600)
         .position_centered()
         .build()
-        .unwrap();
-
-    let ttf = sdl2::ttf::init().unwrap();
-    let font = ttf.load_font(FONT_PATH, 32).unwrap();
-    let text = font
-        .render("Hello World")
-        .blended_wrapped(Color::BLACK, 200)
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
@@ -23,22 +27,28 @@ fn main() {
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
 
+    let lib = ft::Library::init().unwrap();
+    let face = lib.new_face(FONT_PATH, 0).unwrap();
+
+    let texture_creator = canvas.texture_creator();
+    let mut texture_atlas = TextureAtlas::new(&texture_creator, (1024, 1024), face);
+
+    let mut text_doc = TextDoc::new(TEXT.to_owned());
+
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+    let mut last_time = SystemTime::now();
+    let mut frame = 0.0;
+    let mut scroll_velocity = 0;
+    let t = SystemTime::now();
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        let dt = t.elapsed().unwrap().as_millis() as u64;
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        let src = text.rect();
-        canvas
-            .copy(
-                &text.as_texture(&canvas.texture_creator()).unwrap(),
-                src,
-                Rect::new(50, 50, src.width(), src.height()),
-            )
-            .unwrap();
+
+        text_doc.render(&mut texture_atlas, &mut canvas);
         canvas.present();
-        let event = event_pump.wait_event_timeout(1_000u32 / 60);
+        let event = event_pump.wait_event_timeout(1_000u32 / 120);
         if let Some(event) = event {
             match event {
                 Event::Quit { .. }
@@ -46,8 +56,146 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::MouseWheel {
+                    mut y, direction, ..
+                } => {
+                    if direction == MouseWheelDirection::Flipped {
+                        y = -y;
+                    }
+                    y *= 20;
+                    scroll_velocity = y;
+                }
                 _ => {}
             }
+        }
+
+        text_doc.scroll(scroll_velocity as _);
+        scroll_velocity = scroll_velocity * 99 / 100;
+
+        frame += 1.0;
+        let elapsed = last_time.elapsed().unwrap();
+        if elapsed.as_secs() > 1 {
+            let fps = frame / elapsed.as_secs_f32();
+            println!("FPS: {fps}");
+            last_time = SystemTime::now();
+            frame = 0.0;
+        }
+    }
+}
+
+// How to generate font atlas
+// --------------------------
+//
+//  A Store all the chars in Unicode?
+//
+//    There are 150k chars in Unicode.
+//    At 20px, we get 400px per char approx.
+//    150,000 * 400 = 60,000,000
+//    Roughly 2000x2000px atlas.
+//
+//    Won't work well for higher font sizes as we are already at the texture size limit.
+//    Could split into multiple textues.
+//
+//  B For every document, get all the characters in the document
+//    and generate the atlas.
+//
+//    Will have to regenerate the atlas for every text document.
+//    Size of the atlas will be smaller.
+//
+//  C As we process the document, cache the glyph bitmap for every (glyph, font-size)
+//
+//    Check if (char, font-size) is in cache.
+//    If yes, use that
+//    else call freetype to draw it and put it in cache
+//
+
+// How to scroll
+// -------------
+//
+//  A Generate the whole page at once as a texture
+//
+//    Could hit the texture size limit very easily.
+//
+//  B Generate the whole page at once, but split into multiple textures
+//
+//    High memory usage.
+//    Simpler to implement.
+//
+//  C Generate 3 pages (prev, current, next)
+//
+//    When scroll down 0.5 page, (prev = current, current = next, next = render())
+//    Will cause more redraws than previous, but memory efficient.
+//
+
+#[derive(Hash, Eq, PartialEq)]
+struct Glyph {
+    font_size: usize,
+    char: char,
+}
+
+mod texture_atlas;
+
+struct TextDoc {
+    text: String,
+    scroll_offset: i64,
+}
+
+impl TextDoc {
+    fn new(text: String) -> TextDoc {
+        TextDoc {
+            text,
+            scroll_offset: 0,
+        }
+    }
+
+    fn scroll(&mut self, amt: i64) {
+        self.scroll_offset += amt;
+    }
+
+    fn render(&self, texture_atlas: &mut TextureAtlas, canvas: &mut Canvas<Window>) {
+        let (width, height) = canvas.output_size().unwrap();
+        let width = width as i64;
+        let height = height as i64;
+
+        let line_height = texture_atlas.line_height() as i64;
+        let (mut x, mut y) = (0, line_height + self.scroll_offset);
+
+        for c in self.text.chars() {
+            let glyph_info = texture_atlas.get(c, 40).unwrap();
+            let metrics = glyph_info.metrics;
+
+            if c == '\n' || x + (metrics.horiAdvance / 64) > width as i64 {
+                x = 0;
+                y += line_height;
+            };
+
+            if c == '\n' {
+                continue;
+            }
+
+            if y - line_height > height {
+                break;
+            }
+
+            if y >= -line_height {
+                let src = Rect::new(
+                    glyph_info.x as _,
+                    glyph_info.y as _,
+                    glyph_info.width as _,
+                    glyph_info.height as _,
+                );
+
+                let dst = Rect::new(
+                    x as i32 + (metrics.horiBearingX / 64) as i32,
+                    y as i32 - (metrics.horiBearingY / 64) as i32,
+                    glyph_info.width as _,
+                    glyph_info.height as _,
+                );
+
+                canvas.copy(texture_atlas.texture(), src, dst).unwrap();
+            }
+
+            x += metrics.horiAdvance / 64;
         }
     }
 }
---
+++ src/texture_atlas.rs
@@ -0,0 +1,294 @@
+use std::collections::HashMap;
+
+use freetype::bitmap::PixelMode;
+use freetype::face::LoadFlag;
+use freetype::ffi::FT_Glyph_Metrics;
+use freetype::Bitmap;
+use freetype::Face;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
+use sdl2::render::Texture;
+use sdl2::render::TextureAccess;
+use sdl2::render::TextureCreator;
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+pub struct Glyph {
+    pub char: char,
+    pub font_size: u32,
+}
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+pub struct GlyphInfo {
+    pub x: u32,
+    pub y: u32,
+    pub width: u32,
+    pub height: u32,
+    pub metrics: FT_Glyph_Metrics,
+}
+pub struct TextureAtlas<'r> {
+    texture: Texture<'r>,
+    size: (u32, u32),
+    next: (u32, u32),
+    max_height: u32,
+    glyphs: HashMap<Glyph, GlyphInfo>,
+    face: Face,
+}
+
+impl<'r> TextureAtlas<'r> {
+    pub fn new<T: 'r>(
+        texture_creator: &'r TextureCreator<T>,
+        size: (u32, u32),
+        face: Face,
+    ) -> Self {
+        let mut texture = texture_creator
+            .create_texture(
+                PixelFormatEnum::RGBA8888,
+                TextureAccess::Static,
+                size.0,
+                size.1,
+            )
+            .unwrap();
+        texture.set_blend_mode(sdl2::render::BlendMode::Blend);
+        TextureAtlas {
+            texture,
+            size,
+            next: (0, 0),
+            max_height: 0,
+            glyphs: HashMap::new(),
+            face,
+        }
+    }
+
+    pub fn texture(&self) -> &Texture<'r> {
+        &self.texture
+    }
+
+    pub fn line_height(&self) -> i64 {
+        self.face.size_metrics().unwrap().height / 64
+    }
+
+    pub fn get(&mut self, char: char, font_size: u32) -> Option<GlyphInfo> {
+        if let Some(glyph) = self.glyphs.get(&Glyph { char, font_size }) {
+            Some(*glyph)
+        } else {
+            self.face
+                .set_char_size((font_size * 64) as isize, 0, 50, 0)
+                .unwrap();
+            self.face
+                .load_char(char as usize, LoadFlag::RENDER)
+                .unwrap();
+            let glyph = self.face.glyph();
+            let metrics = glyph.metrics();
+            let bitmap = glyph.bitmap();
+            let bitmap_rgba8888 = BitmapRGBA8888::new(&bitmap);
+            let (width, height) = (bitmap_rgba8888.width, bitmap_rgba8888.height);
+            let (x, y) = self.get_slot(width, height)?;
+            let rect = Rect::new(x as _, y as _, width, height);
+
+            self.texture
+                .update(
+                    rect,
+                    &bitmap_rgba8888.pixel_data,
+                    bitmap_rgba8888.pitch as _,
+                )
+                .unwrap();
+
+            let glyph_info = GlyphInfo {
+                x,
+                y,
+                width,
+                height,
+                metrics,
+            };
+
+            self.glyphs.insert(Glyph { char, font_size }, glyph_info);
+            Some(glyph_info)
+        }
+    }
+
+    fn get_slot(&mut self, width: u32, height: u32) -> Option<(u32, u32)> {
+        let (mut x, mut y) = self.next;
+        if x + width > self.size.0 {
+            x = 0;
+            y += self.max_height;
+            self.max_height = 0;
+        }
+
+        if y + height > self.size.1 {
+            return None;
+        }
+
+        self.next = (x + width, y);
+        self.max_height = self.max_height.max(height);
+        Some((x, y))
+    }
+}
+
+struct BitmapRGBA8888 {
+    pixel_data: Vec<u8>,
+    width: u32,
+    height: u32,
+    pitch: u32,
+}
+
+impl BitmapRGBA8888 {
+    pub fn new(bitmap: &Bitmap) -> Self {
+        let width = (bitmap.width() as u32).max(1);
+        let height = (bitmap.rows() as u32).max(1);
+        let bytes_per_pixel = 4 as u32;
+        let pitch = width * bytes_per_pixel;
+
+        let mut pixel_data = vec![0u8; (width * height * bytes_per_pixel) as usize];
+
+        let mut draw = |x: u32, y: u32, color_rgba: (u8, u8, u8, u8)| {
+            pixel_data[(y * width * bytes_per_pixel + x * bytes_per_pixel + 0) as usize] =
+                color_rgba.0;
+            pixel_data[(y * width * bytes_per_pixel + x * bytes_per_pixel + 1) as usize] =
+                color_rgba.1;
+            pixel_data[(y * width * bytes_per_pixel + x * bytes_per_pixel + 2) as usize] =
+                color_rgba.2;
+            pixel_data[(y * width * bytes_per_pixel + x * bytes_per_pixel + 3) as usize] =
+                color_rgba.3;
+        };
+
+        if bitmap.pitch() < 0 {
+            unimplemented!("Negative pitch not impl'd");
+        }
+
+        match bitmap.pixel_mode().unwrap() {
+            PixelMode::None => unreachable!(),
+            PixelMode::Mono => {
+                println!("PixelMode::Mono");
+                let mut i = 0;
+                let mut y = 0;
+                let black = (0, 0, 0, 1);
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() / 8 {
+                        let b = bitmap.buffer()[(i + x) as usize];
+                        let pixels = [
+                            b >> 7 == 1,
+                            b >> 6 == 1,
+                            b >> 5 == 1,
+                            b >> 4 == 1,
+                            b >> 3 == 1,
+                            b >> 2 == 1,
+                            b >> 1 == 1,
+                            b >> 0 == 1,
+                        ];
+                        for (j, p) in pixels.iter().enumerate() {
+                            if *p {
+                                draw(x as u32 + j as u32, y, black);
+                            }
+                        }
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+            PixelMode::Gray => {
+                println!("PixelMode::Gray");
+                let mut i = 0;
+                let mut y = 0;
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() {
+                        let p = bitmap.buffer()[(i + x) as usize];
+                        let color_rgba = (p, p, p, p);
+                        draw(x as _, y, color_rgba);
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+            PixelMode::Gray2 => {
+                println!("PixelMode::Gray2");
+                let mut i = 0;
+                let mut y = 0;
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() / 4 {
+                        let b = bitmap.buffer()[(i + x) as usize];
+                        let pixels = [b >> 6 & 0b11, b >> 4 & 0b11, b >> 2 & 0b11, b >> 0 & 0b11];
+                        for (j, p) in pixels.iter().enumerate() {
+                            let gray = p * 255 / 3;
+                            let color_rgba = (gray, gray, gray, gray);
+                            draw(x as u32 + j as u32, y, color_rgba)
+                        }
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+            PixelMode::Gray4 => {
+                println!("PixelMode::Gray4");
+                let mut i = 0;
+                let mut y = 0;
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() / 2 {
+                        let b = bitmap.buffer()[(i + x) as usize];
+                        let pixels = [b >> 4 & 0b1111, b >> 0 & 0b1111];
+                        for (j, p) in pixels.iter().enumerate() {
+                            let gray = p * 255 / 15;
+                            let color_rgba = (gray, gray, gray, gray);
+                            draw(x as u32 + j as u32, y, color_rgba)
+                        }
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+            PixelMode::Lcd => {
+                println!("PixelMode::Lcd");
+                let mut i = 0;
+                let mut y = 0;
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() {
+                        let (r, g, b) = (
+                            (i + 3 * x) as usize,
+                            (i + 3 * x + 1) as usize,
+                            (i + 3 * x + 2) as usize,
+                        );
+                        let color_rgba = (
+                            bitmap.buffer()[r],
+                            bitmap.buffer()[g],
+                            bitmap.buffer()[b],
+                            255,
+                        );
+                        draw(x as _, y, color_rgba);
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+            PixelMode::LcdV => unimplemented!(),
+            PixelMode::Bgra => {
+                println!("PixelMode::Bgra");
+                let mut i = 0;
+                let mut y = 0;
+                while (i as usize) < bitmap.buffer().len() {
+                    for x in 0..bitmap.width() {
+                        let (b, g, r, a) = (
+                            (i + 4 * x) as usize,
+                            (i + 4 * x + 1) as usize,
+                            (i + 4 * x + 2) as usize,
+                            (i + 4 * x + 3) as usize,
+                        );
+                        let color_rgba = (
+                            bitmap.buffer()[r],
+                            bitmap.buffer()[g],
+                            bitmap.buffer()[b],
+                            bitmap.buffer()[a],
+                        );
+                        draw(x as _, y, color_rgba);
+                    }
+                    i += bitmap.pitch();
+                    y += 1;
+                }
+            }
+        };
+        Self {
+            pixel_data,
+            width,
+            height,
+            pitch,
+        }
+    }
+}
